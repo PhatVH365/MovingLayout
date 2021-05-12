036be9fcc21868e9f4dff8e975400a279ade9d76
PhatVH <hongphat.vu.it@gmail.com>
2021-05-12 15:53:50 +0700

    delete comments

--- app/src/main/java/com/example/movinglayout/ui/floorlayout/FloorLayoutActivity.kt
+++ app/src/main/java/com/example/movinglayout/ui/floorlayout/FloorLayoutActivity.kt
@@ -113,7 +113,6 @@ class FloorLayoutActivity : AppCompatActivity() {
 
     @SuppressLint("ClickableViewAccessibility")
     private fun addTable(table: Table, layout: ViewGroup) {
-//        val tableView = TextView(this)
         val lInflater = LayoutInflater.from(this)
         val cardItem = lInflater.inflate(R.layout.table_card_item, null)
 
@@ -121,11 +120,6 @@ class FloorLayoutActivity : AppCompatActivity() {
             ViewGroup.LayoutParams.WRAP_CONTENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
-
-//        params.apply {
-//            width = table.width * columnSize
-//            height = table.height * columnSize
-//        }
 
         cardItem.apply {
 
@@ -235,101 +229,8 @@ class FloorLayoutActivity : AppCompatActivity() {
                     }
                 }
             }
-
-
         }
 
-//        tableView.apply {
-//            layoutParams = params
-//            x = table.x.toFloat() * columnSize
-//            y = table.y.toFloat() * columnSize
-//            text = "${table.name}, ${table.id}"
-//            isSelected = true
-//            background = ResourcesCompat.getDrawable(resources, R.drawable.item_view_bg, null)
-//            tag = "table-${table.id}"
-//
-//            setOnClickListener {
-//                Toast.makeText(context, "Clicked table ${table.name}", Toast.LENGTH_SHORT).show()
-//            }
-//
-//            setOnLongClickListener {
-//                removeTable(this, table, datLayout)
-//                true
-//            }
-//
-//            setOnTouchListener{v, event ->
-//                when (isEditMode) {
-//                    true -> {
-//                        when (event.action) {
-//                            MotionEvent.ACTION_DOWN -> {
-//                                prevX = event.x
-//                                prevY = event.y
-//
-//                                tableEditMode = if (((event.x - v.width <= dpToPx(50f) && event.x - v.width > 0) || (v.width - event.x <= dpToPx(
-//                                        50f
-//                                    ) && v.width - event.x > 0))
-//                                    && ((event.y - v.height <= dpToPx(50f) && event.y - v.height > 0) || (v.height - event.y <= dpToPx(
-//                                        50f
-//                                    ) && v.height - event.y > 0))
-//                                ) {
-//                                    MODE_RESIZE
-//                                } else {
-//                                    MODE_MOVE
-//                                }
-//                                activePointerId = event.getPointerId(0)
-//                            }
-//                            MotionEvent.ACTION_MOVE -> {
-//                                when (tableEditMode) {
-//                                    MODE_MOVE -> {
-//                                        val pointerIndex = event.findPointerIndex(activePointerId)
-//                                        if (pointerIndex != -1) {
-//                                            val currX = event.getX(pointerIndex)
-//                                            val currY = event.getY(pointerIndex)
-//
-//                                            //di chuyển
-//                                            applyTranslation(v, currX - prevX, currY - prevY)
-//                                        }
-//                                    }
-//                                    MODE_RESIZE -> {
-//                                        v.layoutParams.width = event.x.toInt()
-//                                        v.layoutParams.height = event.y.toInt()
-//                                        v.requestLayout()
-//                                    }
-//                                }
-//                            }
-//                            MotionEvent.ACTION_UP -> {
-//                                val postPos = getMarginBlock(v.x, v.y)
-//                                val postSize = getTableSizeBlock(v.width.toFloat(), v.height.toFloat())
-//
-//                                Timber.i("x = ${postPos[0]}, y = ${postPos[1]}")
-//                                if (postPos[0] < 0) postPos[0] = 0
-//                                if (postPos[0] + floor(v.width / columnSize.toFloat()) > 100) postPos[0] =
-//                                    100 - floor(v.width / columnSize.toFloat()).toInt()
-//                                if (postPos[1] < 0) postPos[1] = 0
-//                                if (postPos[1] + floor(v.height / columnSize.toFloat()) > 100) postPos[1] =
-//                                    100 - floor(v.height / columnSize.toFloat()).toInt()
-//                                v.x = postPos[0] * columnSize.toFloat()
-//                                v.y = postPos[1] * columnSize.toFloat()
-//                                v.layoutParams.width = postSize[0] * columnSize
-//                                v.layoutParams.height = postSize[1] * columnSize
-//                                v.requestLayout()
-//                                table.apply {
-//                                    table.x = postPos[0]
-//                                    table.y = postPos[1]
-//                                    table.width = postSize[0]
-//                                    table.height = postSize[1]
-//                                }
-//                                tableEditMode = MODE_STILL
-//                            }
-//                        }
-//                        true
-//                    }
-//                    false -> {
-//                        false
-//                    }
-//                }
-//            }
-//        }
         layout.addView(cardItem)
     }
 
